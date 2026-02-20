c162be51ae265ebe2694b7e34deae13d752cae59
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-20 22:34:12 +0530

    Python

---
+++ python3.py
@@ -0,0 +1,24 @@
+# --- Arithmetic Operators ---
+a = 15
+b = 4
+
+print(a + b)   # Addition (+): Adds 15 and 4 to get 19
+print(a - b)   # Subtraction (-): Subtracts 4 from 15 to get 11
+print(a * b)   # Multiplication (*): Multiplies 15 by 4 to get 60
+print(a / b)   # Division (/): Divides 15 by 4 to get 3.75
+print(a % b)   # Modulus (%): Returns the remainder of 15 divided by 4, which is 3
+print(a ** b)  # Exponentiation (**): Raises 15 to the power of 4, resulting in 50625
+print(a // b)  # Floor Division (//): Divides 15 by 4 and rounds down to 3
+
+# --- Comparison Operators ---
+x = 10
+y = 20
+
+print(x == y)  # Equal (==): Checks if 10 is equal to 20; returns False
+print(x != y)  # Not Equal (!=): Checks if 10 is not equal to 20; returns True
+print(x > y)   # Greater Than (>): Checks if 10 is greater than 20; returns False
+print(x < y)   # Less Than (<): Checks if 10 is less than 20; returns True
+# Greater Than or Equal To (>=): Checks if 10 is greater than or equal to 10; returns True
+print(x >= 10)
+# Less Than or Equal To (<=): Checks if 20 is less than or equal to 15; returns False
+print(y <= 15)
